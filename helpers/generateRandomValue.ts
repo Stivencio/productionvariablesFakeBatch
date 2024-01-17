6de4db578c76707d533977b71e8f2489ea7ad725
Esteban <egonzalez@emoac.cl>
2024-01-17 13:44:22 -0300

    generateRandomValue function modified to generate a number from a range of values

--- helpers/generateRandomValue.ts
+++ helpers/generateRandomValue.ts
@@ -1,3 +1,6 @@
-export const generateRandomValue = () => {
-    return Math.floor(Math.random() * 100);
+export const generateRandomValue = (min, max) => {
+    if (min >= max) {
+        throw new Error('El valor mínimo debe ser menor que el valor máximo');
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
